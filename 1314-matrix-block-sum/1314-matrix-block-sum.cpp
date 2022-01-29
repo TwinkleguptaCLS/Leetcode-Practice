0897fb6340f22715aba9ef4d827412219bc5006f
Twinkle Gupta <Twinkle.gupta2@q2.com>
2022-01-29 14:12:36 +0530

    Time: 20 ms (34.04%), Space: 9.4 MB (76.05%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.cpp
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -3,25 +3,48 @@ public:
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
          int m = mat.size();  //rows
           int n = mat[0].size(); //cols
-        vector<vector<int>> ans(m);
+        vector<vector<int>> ans(m,vector<int> (n));
+        for(int i=0;i<m;i++)
+        {
+            int sum=0;
+            for(int j=0;j<n;j++)
+            {
+                sum+=mat[i][j];
+                mat[i][j] = sum;
+            }
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            int sum=0;
+            for(int j=0;j<m;j++)
+            {
+                sum+=mat[j][i];
+                mat[j][i] = sum;
+            }
+        }
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                int sum =0;
                 int s_i = max(i-k,0);
                 int e_i = min(i+k,m-1);
                 int s_j = max(j-k,0);
                 int e_j = min(j+k,n-1);
-                for(int i2 = s_i;i2<=e_i;i2++)
+            
+                ans[i][j] = mat[e_i][e_j];
+                if(s_i -1>=0)
                 {
-                    for(int j2= s_j;j2<=e_j;j2++)
-                    {
-                        sum+= mat[i2][j2];
-                    }
+                    ans[i][j] -= mat[s_i-1][e_j];
                 }
-            ans[i].push_back(sum);
-
+                if(s_j -1>=0)
+                {
+                    ans[i][j] -= mat[e_i][s_j-1];
+                }
+                if(s_i-1>=0 && s_j-1>=0)
+                {
+                    ans[i][j] += mat[s_i-1][s_j-1];
+                }
              }
         }
         return ans;
