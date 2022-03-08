cbcc1d08348577c7beefafbbd61eaa7c3f7d94ac
Twinkle Gupta <Twinkle.gupta2@q2.com>
2022-03-08 18:22:19 +0530

    Time: 72 ms (22.39%), Space: 9.7 MB (44.65%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
+    set<vector<int>>ans;
     void allpermute(int i,vector<int>&nums)
     {
-        if(i==nums.size()-1)
+         if(i==nums.size()-1)
         {
-            ans.push_back(nums);
+            ans.insert(nums);
             return;
         }
-        unordered_map<int,int>freq;
         for(int j=i;j<nums.size();j++)
         {
-            if(freq[nums[j]]==0)
-            {
-                swap(nums[j],nums[i]);
-                allpermute(i+1,nums);
-                swap(nums[j],nums[i]);
-            }
-            freq[nums[j]]++;
+            swap(nums[i],nums[j]);
+            allpermute(i+1,nums);
+            swap(nums[i],nums[j]);
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         allpermute(0,nums);
-        return ans;
+        vector<vector<int>>res;
+        for(auto i:ans)
+        {
+            res.push_back(i);
+        }
+        return res;
     }
 };
