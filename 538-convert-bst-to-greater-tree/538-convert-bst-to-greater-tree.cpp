7b0dd5341445f712489bba3ed148425c1cc9c98d
Twinkle Gupta <Twinkle.gupta2@q2.com>
2022-04-16 16:17:46 +0530

    Time: 87 ms (6.77%), Space: 33.5 MB (19.45%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
@@ -11,13 +11,31 @@
  */
 class Solution {
 public:
-    int sum=0;
+   
+    int sum = 0;
+    void globalSum(TreeNode* root)
+    {
+        if(!root) return ;
+        sum+=root->val;
+        globalSum(root->left);
+        globalSum(root->right);
+    }
+
+    TreeNode* helper(TreeNode* root,int &s1)
+    {
+        if(!root) return NULL;
+        helper(root->left,s1);// NULL
+        s1=s1+(root->val);
+        root->val = (sum-s1+root->val);
+        cout<<root->val<<" , ";
+        helper(root->right,s1);
+        return root;
+    }
     TreeNode* convertBST(TreeNode* root) {
         if(!root) return NULL;
-        convertBST(root->right);
-        sum+=root->val;
-        root->val = sum;
-        convertBST(root->left);
-        return root;
+        globalSum(root);
+        int s1 = 0;
+        return helper(root,s1);   
+        
     }
 };
