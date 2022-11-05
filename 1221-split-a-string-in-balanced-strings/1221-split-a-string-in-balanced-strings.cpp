7260084ed3e60944294508d874ab8596df635b59
Twinkle Gupta <Twinkle.gupta2@q2.com>
2022-11-05 09:09:43 +0530

    Time: 4 ms (29.03%), Space: 6.2 MB (38.11%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int ans=0;
+        int cnt=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]== 'R')
+            {
+               cnt++;
+            }
+            if(s[i]=='L')
+            {
+                cnt--;
+            }
+            if(cnt==0)
+            {
+                ans++;
+                cnt=0;
+            }
+        }
+        return ans;
+    }
+};
