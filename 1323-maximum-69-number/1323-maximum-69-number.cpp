4af7d61db5078b8f92fe13492f510dd0a5e7c334
Twinkle Gupta <Twinkle.gupta2@q2.com>
2022-11-05 10:05:58 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (97.45%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+       string str = to_string(num);
+       for(int i=0;i<str.length();i++)
+       {
+           if(str[i]=='6')
+           {
+               str[i]='9';
+               num = stoi(str);
+               break;
+           }
+           
+       }
+        return num;
+    }
+    
+};
